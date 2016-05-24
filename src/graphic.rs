8ddbda374e978107773c11a6bec53c0e1d58d5dd
f-prettyland <fp26@st-andrews.ac.uk>
2016-05-24 13:33:22 +0100

    Fixed tabs

--- src/graphic.rs
+++ src/graphic.rs
@@ -13,53 +13,53 @@ use std::f32;
 
 #[derive(Clone)]
 pub struct Config {
-    size: Float,
-    step: Float,
-    max_iters: Int,
-    escape_radius: Float,
-    with_color: bool,
+	size: Float,
+	step: Float,
+	max_iters: Int,
+	escape_radius: Float,
+	with_color: bool,
 }
 
 impl Default for Config {
-    fn default() -> Self {
-        Config {
-            size: 2.0,
-            step: 0.01,
-            max_iters: 20,
-            escape_radius: 2.0,
-            with_color: false,
-        }
-    }
+	fn default() -> Self {
+		Config {
+			size: 2.0,
+			step: 0.01,
+			max_iters: 20,
+			escape_radius: 2.0,
+			with_color: false,
+		}
+	}
 }
 
 impl Config {
-    pub fn size(mut self, size: Float) -> Self {
-        self.size = size;
-        self
-    }
+	pub fn size(mut self, size: Float) -> Self {
+		self.size = size;
+		self
+	}
 
-    pub fn step(mut self, step: Float) -> Self {
-        self.step = step;
-        self
-    }
+	pub fn step(mut self, step: Float) -> Self {
+		self.step = step;
+		self
+	}
 
-    pub fn max_iters(mut self, iters: Int) -> Self {
-        self.max_iters = iters;
-        self
-    }
+	pub fn max_iters(mut self, iters: Int) -> Self {
+		self.max_iters = iters;
+		self
+	}
 
-    pub fn escape_radius(mut self, radius: Float) -> Self {
-        self.escape_radius = radius;
-        self
-    }
+	pub fn escape_radius(mut self, radius: Float) -> Self {
+		self.escape_radius = radius;
+		self
+	}
 
-    pub fn with_color(mut self) -> Self {
-        self.with_color = true;
-        self
-    }
+	pub fn with_color(mut self) -> Self {
+		self.with_color = true;
+		self
+	}
 
 
-    pub fn gen_loop(&self) {
+	pub fn gen_loop(&self) {
 	let img_dim : Img = ((self.size * 2.0) / self.step) as Img;
 	let mut buffer = image::ImageBuffer::new(img_dim, img_dim);
 
@@ -71,39 +71,39 @@ impl Config {
 	let scl : Float = 255.0/(mx as Float);
 	
 	for y_cnt in (0..(img_dim-1)).rev() {
-	    x = -1.0 * self.size;
-	    for x_cnt in 0..(img_dim-1) {
+		x = -1.0 * self.size;
+		for x_cnt in 0..(img_dim-1) {
 		let tx = tx.clone();
 
-                let conf = self.clone();
+				let conf = self.clone();
 		thread::spawn(move || {
-		    let xy_steps : Int = fracmaths::get_passes_mandelbrot((x, y),
-                                                                          conf.max_iters,
-                                                                          conf.escape_radius);
+			let xy_steps : Int = fracmaths::get_passes_mandelbrot((x, y),
+																		  conf.max_iters,
+																		  conf.escape_radius);
 
-		    let mut rgb = (0,0,0);
-		    if xy_steps != conf.max_iters {
+			let mut rgb = (0,0,0);
+			if xy_steps != conf.max_iters {
 			rgb = (rgb_val(xy_steps, 0),rgb_val(xy_steps, 1), rgb_val(xy_steps, 2));
-		    }
-		    tx.send((x_cnt,y_cnt,rgb.0,rgb.1,rgb.2)).unwrap();
+			}
+			tx.send((x_cnt,y_cnt,rgb.0,rgb.1,rgb.2)).unwrap();
 		});
 		x += self.step;
-	    }
-	    y -= self.step;
+		}
+		y -= self.step;
 	}
 
 	for _ in (0..(img_dim-1)).rev() {
-	    for _ in 0..(img_dim-1) {
+		for _ in 0..(img_dim-1) {
 		let (p,q,r,g,b) = rx.recv().unwrap();
 		buffer.put_pixel(p, q, image::Rgb([r,g,b]));
 		// buffer.put_pixel(p, q, image::Luma([(scl*(z as Float)) as u8]));
-	    }
+		}
 	}
 
 	let file = &mut File::create(&Path::new(&format!("./res/fractal-St{}-Mx{}.png", self.step, self.max_iters))).unwrap();
 	// let _ = image::ImageLuma8(buffer).save(path, image::PNG);
 	let _ = image::ImageRgb8(buffer).save(file, image::PNG);
-    }
+	}
 }
 
 fn rgb_val(steps : Int, scl : Int) -> u8 {
--- src/main.rs
+++ src/main.rs
@@ -10,89 +10,89 @@ use types::*;
 use std::env;
 
 fn main() {
-    //Bound size, Step size, Max passes, Divergence radius
-    let args: Vec<_> = env::args().collect();
-    let program = args[0].clone();
-    
-    let mut opts = getopts::Options::new();
+	//Bound size, Step size, Max passes, Divergence radius
+	let args: Vec<_> = env::args().collect();
+	let program = args[0].clone();
+	
+	let mut opts = getopts::Options::new();
 
-    opts.optflag("h", "help", "Display this help message");
-    opts.optflag("b", "no-color", "Black and white output");
-    opts.optflag("t", "term", "Terminal output (ignores all other args)");
+	opts.optflag("h", "help", "Display this help message");
+	opts.optflag("b", "no-color", "Black and white output");
+	opts.optflag("t", "term", "Terminal output (ignores all other args)");
 
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
-    };
+	let matches = match opts.parse(&args[1..]) {
+		Ok(m) => m,
+		Err(f) => panic!(f.to_string()),
+	};
 
-    if matches.opt_present("h") {
-        print_usage(&program, opts);
-        return;
-    }
+	if matches.opt_present("h") {
+		print_usage(&program, opts);
+		return;
+	}
 
-    if matches.opt_present("t") {
-        terminal::gen_term_loop();
-        return;
-    }
+	if matches.opt_present("t") {
+		terminal::gen_term_loop();
+		return;
+	}
 
-    let settings = matches.free;
+	let settings = matches.free;
 
-    if settings.len() == 4 {
-        if let Ok(configs) = parse_settings(settings) {
-            configs.gen_loop();
-            return;
-        }
-    }
-    print_usage(&program, opts);
+	if settings.len() == 4 {
+		if let Ok(configs) = parse_settings(settings) {
+			configs.gen_loop();
+			return;
+		}
+	}
+	print_usage(&program, opts);
 }
 
 // Should be a nicer way of doing this
 // First attempt at a clean up
 macro_rules! parse_setting {
-    ($setting:ident, $setting_type:ident, $position:expr, $settings:ident) => (
-        let $setting: $setting_type = match $settings[$position].parse() {
-            Ok(value) => value,
-            Err(_) => return Err(SettingsError::from_position($position)),
-        }
-    );
+	($setting:ident, $setting_type:ident, $position:expr, $settings:ident) => (
+		let $setting: $setting_type = match $settings[$position].parse() {
+			Ok(value) => value,
+			Err(_) => return Err(SettingsError::from_position($position)),
+		}
+	);
 }
 
 fn parse_settings(settings: Vec<String>) -> Result<graphic::Config, SettingsError> {
-    parse_setting!(size, Float, 0, settings);
-    parse_setting!(step, Float, 1, settings);
-    parse_setting!(iters, Int, 2, settings);
-    parse_setting!(radius, Float, 3, settings);
+	parse_setting!(size, Float, 0, settings);
+	parse_setting!(step, Float, 1, settings);
+	parse_setting!(iters, Int, 2, settings);
+	parse_setting!(radius, Float, 3, settings);
 
-    Ok(graphic::Config::default()
-       .size(size)
-       .step(step)                            
-       .max_iters(iters)
-       .escape_radius(radius)
-       .with_color())
+	Ok(graphic::Config::default()
+	   .size(size)
+	   .step(step)                            
+	   .max_iters(iters)
+	   .escape_radius(radius)
+	   .with_color())
   }
 
 enum SettingsError {
-    Size,
-    Step,
-    Iters,
-    Radius,
-    Unknown,
+	Size,
+	Step,
+	Iters,
+	Radius,
+	Unknown,
 }
 
 impl SettingsError {
-    fn from_position(position: usize) -> Self {
-        match position {
-            0 => SettingsError::Size,
-            1 => SettingsError::Step,
-            2 => SettingsError::Iters,
-            3 => SettingsError::Radius,
-            _ => SettingsError::Unknown,
-        } 
-    }
+	fn from_position(position: usize) -> Self {
+		match position {
+			0 => SettingsError::Size,
+			1 => SettingsError::Step,
+			2 => SettingsError::Iters,
+			3 => SettingsError::Radius,
+			_ => SettingsError::Unknown,
+		} 
+	}
 }
 
 fn print_usage(program: &str, opts: getopts::Options) {
-    let brief = format!("Usage: {} SIZE STEP MAX_ITERATIONS ESCAPE_RADIUS [options]", program);
-    print!("{}", opts.usage(&brief));
+	let brief = format!("Usage: {} SIZE STEP MAX_ITERATIONS ESCAPE_RADIUS [options]", program);
+	print!("{}", opts.usage(&brief));
 }
 
