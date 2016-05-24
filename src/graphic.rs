f67710bc8f5a86cf2609474fc26d9c53911e38e7
f-prettyland <fp26@st-andrews.ac.uk>
2016-05-24 13:28:42 +0100

    working on bw & colour

--- src/graphic.rs
+++ src/graphic.rs
@@ -16,11 +16,12 @@ pub fn gen_loop(size : Float, stp : Float, mx : Int, dr : Float, bw : bool){
 	let img_dim : Img = ((size*2.0)/stp) as Img;
 	let mut buffer = image::ImageBuffer::new(img_dim, img_dim);
 
-	// let scl : Float = 255.0/(mx as Float);
+	// 
 	let mut x : Float;
 	let mut y : Float = size;
 
 	let (tx, rx) = mpsc::channel();
+	let scl : Float = 255.0/(mx as Float);
 	
 	for y_cnt in (0..(img_dim-1)).rev() {
 		x = -1.0*size;
@@ -28,11 +29,16 @@ pub fn gen_loop(size : Float, stp : Float, mx : Int, dr : Float, bw : bool){
 			let tx = tx.clone();
 			thread::spawn(move || {
 						let xy_steps : Int = fracmaths::get_passes_mandelbrot((x, y), mx, dr);
-						let mut rgb = (0,0,0);
-						if xy_steps != mx {
-							rgb = (rgb_val(xy_steps, 0),rgb_val(xy_steps, 1), rgb_val(xy_steps, 2));
+						
+						if !bw {
+							let mut rgb = (0,0,0);
+							if xy_steps != mx {
+								rgb = (rgb_val(xy_steps, 0),rgb_val(xy_steps, 1), rgb_val(xy_steps, 2));
+							}
+							tx.send((x_cnt,y_cnt,rgb.0,rgb.1,rgb.2)).unwrap();
+						}else{
+							tx.send((x_cnt,y_cnt,(scl*(xy_steps as Float)) as u8)).unwrap();
 						}
-						tx.send((x_cnt,y_cnt,rgb.0,rgb.1,rgb.2)).unwrap();
 					});
 			x += stp;
 		}
@@ -41,15 +47,24 @@ pub fn gen_loop(size : Float, stp : Float, mx : Int, dr : Float, bw : bool){
 
 	for _ in (0..(img_dim-1)).rev() {
 		for _ in 0..(img_dim-1) {
-			let (p,q,r,g,b) = rx.recv().unwrap();
-			buffer.put_pixel(p, q, image::Rgb([r,g,b]));
-			// buffer.put_pixel(p, q, image::Luma([(scl*(z as Float)) as u8]));
+			if !bw {
+				let (p,q,r,g,b) = rx.recv().unwrap();
+				buffer.put_pixel(p, q, image::Rgb([r,g,b]));
+
+			}else{
+				let (p,q,z) = rx.recv().unwrap();
+				buffer.put_pixel(p, q, image::Luma([z]));
+			}
 		}
 	}
 
 	let file = &mut File::create(&Path::new(&format!("./res/fractal-St{}-Mx{}.png", stp, mx))).unwrap();
-	// let _ = image::ImageLuma8(buffer).save(path, image::PNG);
-	let _ = image::ImageRgb8(buffer).save(file, image::PNG);
+	
+	if bw {
+		let _ = image::ImageLuma8(buffer).save(file, image::PNG);
+	}else{
+		let _ = image::ImageRgb8(buffer).save(file, image::PNG);
+	}
 }
 
 fn rgb_val(steps : Int, scl : Int) -> u8 {
@@ -57,3 +72,8 @@ fn rgb_val(steps : Int, scl : Int) -> u8 {
 		((steps as Float)*(20.0*(scl as Float)/(f32::consts::PI))).cos()) as Float
 		+255.0) as u8
 }
+
+
+// pub fn gif_loop(size : Float, stp : Float, mx : Int, dr : Float, bw : bool, zoom : Int){
+
+// }
