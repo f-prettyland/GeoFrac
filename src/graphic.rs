b10592480732e5a49df37cead1c0534283a2940c
f-prettyland <fp26@st-andrews.ac.uk>
2016-06-02 16:29:03 +0100

    Gif frames made, need to increase iterations with each zoom

--- src/graphic.rs
+++ src/graphic.rs
@@ -11,65 +11,79 @@ use types::*;
 
 #[derive(Clone)]
 pub struct Config {
-    size: Float,
-    step: Float,
-    max_iters: Int,
-    escape_radius: Float,
-    with_color: bool,
-    filename: String,
+	size: Float,
+	step: Float,
+	max_iters: Int,
+	escape_radius: Float,
+	with_color: bool,
+	filename: String,
+	begin_x: Float,
+	begin_y: Float,
 }
 
 impl Config {
-    pub fn new(size: Float, step: Float, iters: Int, radius: Float) -> Self {
-        Config {
-            size: size,
-            step: step,
-            max_iters: iters,
-            escape_radius: radius,
+	pub fn new(size: Float, step: Float, iters: Int, radius: Float) -> Self {
+		Config {
+			size: size,
+			step: step,
+			max_iters: iters,
+			escape_radius: radius,
 
-            // TODO: Make modifiable
-            with_color: true,
-            filename: "".to_string(),
-        } 
-    }
-    
-    pub fn size(mut self, size: Float) -> Self {
+			// TODO: Make modifiable
+			with_color: true,
+			filename: "".to_string(),
+			begin_x: -1.0 * size,
+			begin_y: size,
+		} 
+	}
+	
+	pub fn size(mut self, size: Float) -> Self {
 	self.size = size;
 	self
-    }
+	}
 
-    pub fn step(mut self, step: Float) -> Self {
+	pub fn step(mut self, step: Float) -> Self {
 	self.step = step;
 	self
-    }
+	}
 
-    pub fn max_iters(mut self, iters: Int) -> Self {
+	pub fn max_iters(mut self, iters: Int) -> Self {
 	self.max_iters = iters;
 	self
-    }
+	}
 
-    pub fn escape_radius(mut self, radius: Float) -> Self {
+	pub fn escape_radius(mut self, radius: Float) -> Self {
 	self.escape_radius = radius;
 	self
-    }
+	}
 
-    pub fn greyscale(mut self) -> Self {
+	pub fn greyscale(mut self) -> Self {
 	self.with_color = false;
 	self
-    }
+	}
 
-    pub fn filename(mut self, filename : String) -> Self {
-        self.filename = filename;
+	pub fn filename(mut self, filename : String) -> Self {
+		self.filename = filename;
 	self
-    }
+	}
 
-    pub fn run(&self) {
+	pub fn begin_x(mut self, x : Float) -> Self {
+	self.begin_x = x;
+	self
+	}
+
+	pub fn begin_y(mut self, y : Float) -> Self {
+	self.begin_y = y;
+	self
+	}
+
+	pub fn run(&self) {
 	let img_dim : Img = ((self.size * 2.0) / self.step) as Img;
 	let mut buffer = image::ImageBuffer::new(img_dim, img_dim);
 
 	//Coordinate variable initialisation
 	let mut x : Float;
-	let mut y : Float = self.size;
+	let mut y : Float = self.begin_y;
 
 	//Channel creation for threads to pass back colours
 	let (tx, rx) = mpsc::channel();
@@ -81,131 +95,152 @@ impl Config {
 	
 	//Y pixel count
 	for y_cnt in (0..(img_dim-1)).rev() {
-	    //x coordinate
-	    x = -1.0 * self.size;
+		//x coordinate
+		x = self.begin_x;
 
-	    //X pixel count
-	    for x_cnt in 0..(img_dim-1) {
-		//Clone sender for channel
-		let tx = tx.clone();
-		//If not coloured
-		if !self.with_color {
-		    //Creates greyscale thread
-		    thread::spawn(move || {
-			let xy_steps : Int = fracmaths::get_passes_mandelbrot((x, y), mx, er);
-			//Calculate colour value for
-			tx.send((x_cnt,y_cnt,((scl*(xy_steps as Float)) as u8),None,None)).unwrap();
-		    });
-		} else {
-		    thread::spawn(move || {
-			let xy_steps : Int = fracmaths::get_passes_mandelbrot((x, y), mx, er);
-			let mut rgb = (0,0,0);
-			if xy_steps != mx {
-			    rgb = (rgb_val(xy_steps, 0),rgb_val(xy_steps, 1), rgb_val(xy_steps, 2));
+		//X pixel count
+		for x_cnt in 0..(img_dim-1) {
+			//Clone sender for channel
+			let tx = tx.clone();
+			//If not coloured
+			if !self.with_color {
+				//Creates greyscale thread
+				thread::spawn(move || {
+				let xy_steps : Int = fracmaths::get_passes_mandelbrot((x, y), mx, er);
+				//Calculate colour value for
+				tx.send((x_cnt,y_cnt,((scl*(xy_steps as Float)) as u8),None,None)).unwrap();
+				});
+			} else {
+				thread::spawn(move || {
+				let xy_steps : Int = fracmaths::get_passes_mandelbrot((x, y), mx, er);
+				let mut rgb = (0,0,0);
+				if xy_steps != mx {
+					rgb = (rgb_val(xy_steps, 0),rgb_val(xy_steps, 1), rgb_val(xy_steps, 2));
+				}
+				tx.send((x_cnt,y_cnt,rgb.0,Some(rgb.1),Some(rgb.2))).unwrap();
+				});
 			}
-			tx.send((x_cnt,y_cnt,rgb.0,Some(rgb.1),Some(rgb.2))).unwrap();
-		    });
+			x += self.step;
 		}
-		x += self.step;
-	    }
-	    y -= self.step;
+		y -= self.step;
 	}
 
 	for _ in (0..(img_dim-1)).rev() {
-	    for _ in 0..(img_dim-1) {
+		for _ in 0..(img_dim-1) {
 		let (p,q,r,maybeg,maybeb) = rx.recv().unwrap();
 		match maybeg {
-		    Some(g) if maybeb.is_some() =>
+			Some(g) if maybeb.is_some() =>
 			buffer.put_pixel(p, q, image::Rgb([r,g,maybeb.unwrap()])),
-		    _	=> {
+			_	=> {
 			buffer.put_pixel(p, q, image::Rgb([r,r,r]));
-		    }
+			}
 		}
-	    }
+		}
 	}
 
 	let b = match self.with_color{
-	    true => "",
-	    false=> "-bw",
+		true => "",
+		false=> "-bw",
 	};
 
 	let mut filename = &format!("./res/fractal-St{}-Mx{}{}.png", self.step, self.max_iters,b);
 	if self.filename != "" {
-	    filename = &self.filename;
+		filename = &self.filename;
 	}
 	
 	let file = &mut File::create(&Path::new(filename)).unwrap();
 	let _ = image::ImageRgb8(buffer).save(file, image::PNG);
-    }
+	}
 
 
 }
 fn rgb_val(steps : Int, scl : Int) -> u8 {
-    ((-255.0*
-      ((steps as Float)*(20.0*(scl as Float)/(f32::consts::PI))).cos()) as Float
-     +255.0) as u8
+	((-255.0*
+	  ((steps as Float)*(20.0*(scl as Float)/(f32::consts::PI))).cos()) as Float
+	 +255.0) as u8
 }
 
 pub struct GifConfig {
-    init_frame: Config,
-    zoom: Float,
-    z_step: Float,
-    z_centre_x: Float,
-    z_centre_y: Float,
+	init_frame: Config,
+	zoom: Float,
+	z_step: Float,
+	z_centre_x: Float,
+	z_centre_y: Float,
 }
 
 impl GifConfig {
-    pub fn new(generator: Config, zoom: Float, z_step: Float, x: Float, y: Float) -> Self {
-        GifConfig {
-            init_frame: generator,
-            zoom: zoom,
-            z_step: z_step,
-            z_centre_x: x,
-            z_centre_y: y,
-        }
-    }
-    
-    pub fn init_frame(mut self, init: Config) -> Self {
+	pub fn new(generator: Config, zoom: Float, z_step: Float, x: Float, y: Float) -> Self {
+		GifConfig {
+			init_frame: generator,
+			zoom: zoom,
+			z_step: z_step,
+			z_centre_x: x,
+			z_centre_y: y,
+		}
+	}
+	
+	pub fn init_frame(mut self, init: Config) -> Self {
 	self.init_frame = init;
 	self
-    }
-
-    pub fn zoom(mut self, zoom: Float) -> Self {
-	self.zoom = zoom;
-	self
-    }
-
-    pub fn z_step(mut self, step: Float) -> Self {
-	self.z_step = step;
-	self
-    }
-
-    pub fn z_centre_x(mut self, centre: Float) -> Self {
-	self.z_centre_x = centre;
-	self
-    }
-
-    pub fn z_centre_y(mut self, centre: Float) -> Self {
-	self.z_centre_y = centre;
-	self
-    }
-
-
-    pub fn run(&mut self){
-        println!("generating...");
-	let mut curr_z : Float = 1.0;
-	let mut count = 0;
-	loop{
-	    count+=1;
-	    self.init_frame.filename = format!("./gif/{}.png",count);
-	    self.init_frame.size = (1.0/curr_z)*self.init_frame.size;
-	    self.init_frame.step = (1.0/curr_z)*self.init_frame.step;
-	    self.init_frame.run();
-	    curr_z += self.z_step;
-	    if curr_z > self.zoom {
-		break;
-	    }
 	}
 
-    }
+	pub fn zoom(mut self, zoom: Float) -> Self {
+	self.zoom = zoom;
+	self
+	}
+
+	pub fn z_step(mut self, step: Float) -> Self {
+	self.z_step = step;
+	self
+	}
+
+	pub fn z_centre_x(mut self, centre: Float) -> Self {
+	self.z_centre_x = centre;
+	self
+	}
+
+	pub fn z_centre_y(mut self, centre: Float) -> Self {
+	self.z_centre_y = centre;
+	self
+	}
+
+
+	pub fn run(&mut self){
+		println!("generating...");
+	let mut curr_z : Float = 1.0;
+	let mut count = 0;
+	let orig_size = self.init_frame.size;
+	loop{
+		count+=1;
+		self.init_frame.filename = format!("./gif/{}.png",count);
+
+		self.init_frame.run();
+		curr_z += self.z_step;
+		if curr_z > self.zoom {
+		break;
+		}
+
+		self.init_frame.size = (1.0/curr_z)*self.init_frame.size;
+		self.init_frame.step = (1.0/curr_z)*self.init_frame.step;
+
+		// TODO if |zcentre| > size/2 then check these, else ignore
+		let maxx = if self.z_centre_x+(self.init_frame.size) > orig_size {orig_size} else {self.z_centre_x+(self.init_frame.size)};
+		let minx = if self.z_centre_x-(self.init_frame.size) < (-1.0*orig_size) {-1.0*orig_size} else {self.z_centre_x-(self.init_frame.size)};
+
+		let maxy = if self.z_centre_y+(self.init_frame.size) > orig_size {orig_size} else {self.z_centre_y+(self.init_frame.size)};
+		let miny = if self.z_centre_y-(self.init_frame.size) < (-1.0*orig_size) {-1.0*orig_size} else {self.z_centre_y-(self.init_frame.size)};
+
+		let mut beg_x = minx;
+		if maxx == orig_size {
+			beg_x = orig_size-(2.0*self.init_frame.size);
+		}
+
+		let mut beg_y = maxy;
+		if miny == (-1.0*orig_size) {
+			beg_y = (-1.0*orig_size)+(2.0*self.init_frame.size);
+		}
+
+	}
+
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -9,74 +9,74 @@ mod graphic;
 use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let options = [
-        Arg::with_name("size")
-            .help("TODO: size help")
-            .index(1)
-            .required(true),
-        Arg::with_name("step")
-            .help("TODO: step help")
-            .index(2)
-            .required(true),
-        Arg::with_name("iterations")
-            .help("TODO: iterations help")
-            .index(3)
-            .required(true),
-        Arg::with_name("radius")
-            .help("TODO: radius help")
-            .index(4)
-            .required(true),
-        Arg::with_name("greyscale")
-            .help("TODO: greyscale help")
-            .short("g")
-            .long("grey"),
-        Arg::with_name("output")
-            .help("TODO: output help")
-            .takes_value(true)
-            .short("o")
-            .long("output")
-    ];
+	let options = [
+		Arg::with_name("size")
+			.help("TODO: size help")
+			.index(1)
+			.required(true),
+		Arg::with_name("step")
+			.help("TODO: step help")
+			.index(2)
+			.required(true),
+		Arg::with_name("iterations")
+			.help("TODO: iterations help")
+			.index(3)
+			.required(true),
+		Arg::with_name("radius")
+			.help("TODO: radius help")
+			.index(4)
+			.required(true),
+		Arg::with_name("greyscale")
+			.help("TODO: greyscale help")
+			.short("g")
+			.long("grey"),
+		Arg::with_name("output")
+			.help("TODO: output help")
+			.takes_value(true)
+			.short("o")
+			.long("output")
+	];
 
-    let matches = App::new("GeoFrac")
-        .subcommands(vec![
-            SubCommand::with_name("still")
-                .about("Generates a still image")
-                .args(&options),
-            SubCommand::with_name("gif")
-                .about("Generates an animated gif")
-                .args(&options)
-                .args(&[
-                    Arg::with_name("zoom")
-                        .help("TODO: zoom help")
-                        .index(5)
-                        .required(true),
-                     Arg::with_name("zstep")
-                        .help("TODO: zstep help")
-                        .index(6)
-                        .required(true),
-                    Arg::with_name("x")
-                        .help("TODO: x help")
-                        .index(7)
-                        .required(true),
-                    Arg::with_name("y")
-                        .help("TODO: y help")
-                        .index(8)
-                        .required(true),
-                ]),
-            SubCommand::with_name("term")
-                .about("Outputs to terminal")
-                .help("TODO: terminal help")
-        ]).get_matches();
+	let matches = App::new("GeoFrac")
+		.subcommands(vec![
+			SubCommand::with_name("still")
+				.about("Generates a still image")
+				.args(&options),
+			SubCommand::with_name("gif")
+				.about("Generates an animated gif")
+				.args(&options)
+				.args(&[
+					Arg::with_name("zoom")
+						.help("TODO: zoom help")
+						.index(5)
+						.required(true),
+					 Arg::with_name("zstep")
+						.help("TODO: zstep help")
+						.index(6)
+						.required(true),
+					Arg::with_name("x")
+						.help("TODO: x help")
+						.index(7)
+						.required(true),
+					Arg::with_name("y")
+						.help("TODO: y help")
+						.index(8)
+						.required(true),
+				]),
+			SubCommand::with_name("term")
+				.about("Outputs to terminal")
+				.help("TODO: terminal help")
+		]).get_matches();
 
 
 
-    match matches.subcommand_name() {
-        Some("still") => run_still(&matches.subcommand_matches("still").unwrap()),
-        Some("gif") => run_gif(&matches.subcommand_matches("gif").unwrap()),
-        Some("term") => terminal::gen_term_loop(),
-        None => println!("None"),
-        _ => println!("Not recognised"),
-    }
+	match matches.subcommand_name() {
+		Some("still") => run_still(&matches.subcommand_matches("still").unwrap()),
+		Some("gif") => run_gif(&matches.subcommand_matches("gif").unwrap()),
+		Some("term") => terminal::gen_term_loop(),
+		None => println!("None"),
+		_ => println!("Not recognised"),
+	}
 }
 
 fn run_still(matches: &clap::ArgMatches) {
@@ -84,32 +84,32 @@ fn run_still(matches: &clap::ArgMatches) {
 }
 
 fn frame_setup(matches: &clap::ArgMatches) -> graphic::Config {
-    let size = value_t!(matches, "size", f32).unwrap_or_else(|e| e.exit());
-    let step = value_t!(matches, "step", f32).unwrap_or_else(|e| e.exit());
-    let iter = value_t!(matches, "iterations", i32).unwrap_or_else(|e| e.exit());
-    let radius = value_t!(matches, "radius", f32).unwrap_or_else(|e| e.exit());
+	let size = value_t!(matches, "size", f32).unwrap_or_else(|e| e.exit());
+	let step = value_t!(matches, "step", f32).unwrap_or_else(|e| e.exit());
+	let iter = value_t!(matches, "iterations", i32).unwrap_or_else(|e| e.exit());
+	let radius = value_t!(matches, "radius", f32).unwrap_or_else(|e| e.exit());
 
-    let mut generator = graphic::Config::new(size, step, iter, radius);
+	let mut generator = graphic::Config::new(size, step, iter, radius);
 
-    if matches.is_present("greyscale") {
-        generator = generator.greyscale();
-    }
+	if matches.is_present("greyscale") {
+		generator = generator.greyscale();
+	}
 
-    if matches.is_present("output") {
-        generator = generator.filename(matches.value_of("output").unwrap().to_string());
-    } 
+	if matches.is_present("output") {
+		generator = generator.filename(matches.value_of("output").unwrap().to_string());
+	} 
 
-    generator
+	generator
 }
 
 fn run_gif(matches: &clap::ArgMatches) {
-    let zoom = value_t!(matches, "zoom", f32).unwrap_or_else(|e| e.exit());
-    let zstep = value_t!(matches, "zstep", f32).unwrap_or_else(|e| e.exit());
-    let x = value_t!(matches, "x", f32).unwrap_or_else(|e| e.exit());
-    let y = value_t!(matches, "y", f32).unwrap_or_else(|e| e.exit());
+	let zoom = value_t!(matches, "zoom", f32).unwrap_or_else(|e| e.exit());
+	let zstep = value_t!(matches, "zstep", f32).unwrap_or_else(|e| e.exit());
+	let x = value_t!(matches, "x", f32).unwrap_or_else(|e| e.exit());
+	let y = value_t!(matches, "y", f32).unwrap_or_else(|e| e.exit());
  
-    let frame_generator = frame_setup(matches);
+	let frame_generator = frame_setup(matches);
 
-    graphic::GifConfig::new(frame_generator, zoom, zstep, x, y).run();
+	graphic::GifConfig::new(frame_generator, zoom, zstep, x, y).run();
 }
 
