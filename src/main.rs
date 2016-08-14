11030ba7774b794e57cdfadf01915e4b2fd38e61
jac32 <jac32@st-andrews.ac.uk>
2016-08-14 19:22:39 +0100

    Improving help messages.
    Several still remaining.

--- src/main.rs
+++ src/main.rs
@@ -15,87 +15,87 @@ use config::FromMatches;
 
 fn main() {
 
-	let options = [
-		Arg::with_name("x")
-			.help("TODO: x help")
-			.required(true),
-		Arg::with_name("y")
-			.help("TODO: y help")
-			.required(true),
-		Arg::with_name("size")
-			.help("TODO: size help")
-			.required(true),
-		Arg::with_name("step")
-			.help("TODO: step help")
-			.required(true),
-		Arg::with_name("iterations")
-			.help("TODO: iterations help")
-			.required(true),
-		Arg::with_name("radius")
-			.help("TODO: radius help")
-				.takes_value(true)
-			.short("r")
-				.long("radius"),
-		Arg::with_name("greyscale")
-			.help("TODO: greyscale help")
-			.short("g")
-			.long("grey"),
-		Arg::with_name("output")
-			.help("TODO: output help")
-			.takes_value(true)
-			.short("o")
-			.long("output")
-	];
+    let options = [
+	Arg::with_name("x")
+	    .help("X coordinate for image center point.")
+	    .required(true),
+	Arg::with_name("y")
+	    .help("Y coordinate for image center point.")
+	    .required(true),
+	Arg::with_name("size")
+	    .help("Side length of area to be rendered.")
+	    .required(true),
+	Arg::with_name("step")
+	    .help("TODO: step help")
+	    .required(true),
+	Arg::with_name("iterations")
+	    .help("TODO: iterations help")
+	    .required(true),
+	Arg::with_name("radius")
+	    .help("TODO: radius help")
+	    .takes_value(true)
+	    .short("r")
+	    .long("radius"),
+	Arg::with_name("greyscale")
+	    .help("Disable color rendering.")
+	    .short("g")
+	    .long("grey"),
+	Arg::with_name("output")
+	    .help("Specify path to save output file.")
+	    .takes_value(true)
+	    .short("o")
+	    .long("output")
+    ];
 
-	let app = App::new("GeoFrac")
+    let app = App::new("GeoFrac")
 	.subcommands(vec![
-		SubCommand::with_name("still")
+	    SubCommand::with_name("still")
 		.about("Generates a still image")
 		.args(&options),
-		SubCommand::with_name("gif")
+	    SubCommand::with_name("gif")
 		.about("Generates an animated gif")
 		.args(&options)
 		.args(&[
-			Arg::with_name("zoom")
-				.help("TODO: zoom help")
-				.required(true),
-			Arg::with_name("zstep")
-				.help("TODO: zstep help")
-				.required(true)	
+		    Arg::with_name("zoom")
+			.help("TODO: zoom help")
+			.required(true),
+		    Arg::with_name("zstep")
+			.help("TODO: zstep help")
+			.required(true)	
 		]),
-		SubCommand::with_name("term")
+	    SubCommand::with_name("term")
 		.about("Outputs to terminal")
 		.help("TODO: terminal help")
 	]);
 
 
-	// TODO: Attempt to remove cloning
-	
-	let clone = app.clone();
-	let matches = app.get_matches();
-	
-	match matches.subcommand_name() {
-		Some("term") => { terminal::gen_term_loop() },
-		Some("still") => {
-			let options = &matches.subcommand_matches("still").unwrap();
-			run_still(options);
-		},
-		Some("gif") => {
-			let options = &matches.subcommand_matches("gif").unwrap();
-			if let Err(_) = run_gif(options) {
-				println!("Could not create output directory: \"/gif\"");
-			};
-		},
-		_ => { clone.print_help().unwrap(); }
-	}
+    // TODO: Attempt to remove cloning
+    
+    let clone = app.clone();
+    let matches = app.get_matches();
+    
+    match matches.subcommand_name() {
+	Some("term") => { terminal::gen_term_loop() },
+	Some("still") => {
+	    let options = &matches.subcommand_matches("still").unwrap();
+	    run_still(options);
+	},
+	Some("gif") => {
+	    let options = &matches.subcommand_matches("gif").unwrap();
+	    if let Err(_) = run_gif(options) {
+		println!("Could not create output directory: \"/gif\"");
+	    };
+	},
+	_ => { clone.print_help().unwrap(); }
+    }
 }
 
 fn run_still(matches: &clap::ArgMatches) {
-	config::Config::from_matches(matches).run()
+    config::Config::from_matches(matches).run()
 }
 
 fn run_gif(matches: &clap::ArgMatches) -> io::Result<()> {
-	try!(fs::create_dir_all(config::GIF_OUT_DIR));
-	config::GifConfig::from_matches(matches).run();
-	Ok(())
+    try!(fs::create_dir_all(config::GIF_OUT_DIR));
+    config::GifConfig::from_matches(matches).run();
+    Ok(())
 }
